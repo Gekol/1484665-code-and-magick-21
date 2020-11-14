ed626fd4ab2cbba21d19461a04f016c2104af503
Gekol <george.sokolovsky2001@gmail.com>
2020-11-14 16:38:27 +0200

    module 6 done

--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,6 @@
 'use strict';
 
 (function () {
-  const NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-  const SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
   const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
   const EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
   const FIREBALL_COLORS = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
@@ -67,41 +65,38 @@
     return elems[generateRandomInt(0, elems.length - 1)];
   }
 
-  function generateCharacterName() {
-    const name = NAMES[generateRandomInt(0, NAMES.length - 1)];
-    const surname = SURNAMES[generateRandomInt(0, SURNAMES.length - 1)];
-    return generateRandomInt(0, 1) > 0 ? `${name} ${surname}` : `${surname} ${name}`;
-  }
-
-  function generateWizardsData() {
-    let characters = [];
-
-    for (let i = 0; i < CHARACTERS_COUNT; i++) {
-      characters.push({
-        name: generateCharacterName(),
-        coatColor: COAT_COLORS[generateRandomInt(0, COAT_COLORS.length - 1)],
-        eyesColor: EYES_COLORS[generateRandomInt(0, EYES_COLORS.length - 1)]
-      });
-    }
-    return characters;
-  }
-
   function generateWizardElem(data) {
     const wizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
     const newElem = wizardTemplate.cloneNode(true);
     newElem.querySelector(`.setup-similar-label`).textContent = data.name;
-    newElem.querySelector(`.wizard-coat`).style.fill = data.coatColor;
-    newElem.querySelector(`.wizard-eyes`).style.fill = data.eyesColor;
+    newElem.querySelector(`.wizard-coat`).style.fill = data.colorCoat;
+    newElem.querySelector(`.wizard-eyes`).style.fill = data.colorEyes;
     return newElem;
   }
 
-  function generateWizardElems() {
+  function successHandler(wizards) {
     const fragment = document.createDocumentFragment();
-    for (let data of generateWizardsData()) {
-      fragment.appendChild(generateWizardElem(data));
+    for (let i = 0; i < CHARACTERS_COUNT; i++) {
+      fragment.appendChild(generateWizardElem(wizards[i]));
     }
     const wizardsList = document.querySelector(`.setup-similar-list`);
     wizardsList.appendChild(fragment);
+  }
+
+  function errorHandler(errorMessage) {
+    let node = document.createElement(`div`);
+    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
+    node.style.position = `absolute`;
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = `30px`;
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  }
+
+  function generateWizardElems() {
+    window.backend.load(successHandler, errorHandler);
   }
 
   function main() {
